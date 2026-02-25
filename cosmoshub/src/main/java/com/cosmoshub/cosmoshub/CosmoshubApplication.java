70e9fff55d60e42dd58e91dc66b092eb7a2d6eb0
Lucas Guerin <lucas.guerin.pro31@gmail.com>
2026-02-25 12:29:03 +0100

    Initial commit

---
+++ cosmoshub/src/main/java/com/cosmoshub/cosmoshub/CosmoshubApplication.java
@@ -0,0 +1,13 @@
+package com.cosmoshub.cosmoshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoshubApplication.class, args);
+	}
+
+}
---
+++ cosmoshub/src/test/java/com/cosmoshub/cosmoshub/CosmoshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmoshub.cosmoshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
